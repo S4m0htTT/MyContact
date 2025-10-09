4d0651e23718a85d638a519bc4dd22c296b4eeb3
S4m0htTT <thomashenry7750@gmail.com>
2025-10-09 11:22:33 +0200

    fix: body-parser error

--- back/src/server/server.ts
+++ back/src/server/server.ts
@@ -2,7 +2,6 @@ import express, {Express} from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 import setRoutes from "./routes.js";
 import morgan from "morgan";
 import swaggerUi from 'swagger-ui-express';
@@ -13,7 +12,7 @@ const swaggerDocs = YAML.load("./swagger.yaml")
 const server: Express = express();
 server.use(helmet());
 server.use(cookieParser());
-server.use(bodyParser.json({ limit: '100mb'}));
+server.use(express.json({ limit: '100mb'}));
 server.use(express.urlencoded({ limit: '100mb', extended: true }));
 server.use(cors());
 server.use(morgan('dev'));
